3af9a2438e48f2ec325dbf3e17cb52d8a04ca3a5
longvv <longvv@bap.jp>
2020-04-17 22:54:43 +0700

    init project

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.13.0"
+
+set :application, "docker_app"
+set :repo_url, "git@github.com:vovanlong/docker-app.git"
+
+set :pty, true
+set :linked_files, %w(config/database.yml config/application.yml)
+set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads)
+set :assets_prefix, "prepackaged-assets"
+set :assets_manifests, ['app/assets/config/manifest.js']
+set :rails_assets_groups, :assets
+set :normalize_asset_timestamps, %w{public/images public/javascripts public/stylesheets}
+set :keep_releases, 5
+set :puma_rackup, -> { File.join(current_path, "config.ru") }
+set :puma_state, -> { "#{shared_path}/tmp/pids/puma.state" }
+set :puma_pid, -> { "#{shared_path}/tmp/pids/puma.pid" }
+set :puma_bind, -> { "unix://#{shared_path}/tmp/sockets/puma.sock" }
+set :puma_conf, -> { "#{shared_path}/puma.rb" }
+set :puma_access_log, -> { "#{shared_path}/log/puma_access.log" }
+set :puma_error_log, -> { "#{shared_path}/log/puma_error.log" }
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, "production"))
+set :puma_threads, [0, 8]
+set :puma_workers, 0
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+set :puma_preload_app, false
